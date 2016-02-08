961bcc5f56d994ee53493ca07b2b29f68d3ce0a0
CadenArmstrong <cadenarmstrong@gmail.com>
2016-02-08 18:24:34 -0500

    Update to transitmodel header for function definitions and input keys. Update for test version of simutrans run from C.

--- simutrans.cpp
+++ simutrans.cpp
@@ -1,6 +1,24 @@
 #include "transitmodel.h"
+#include "simplemodel.cpp"
 
 int main(int argc, char *argv[]){
+	double star_params[] = {350,0.5,0.6,0.1,0.95}; // grid size, limb1, limb2, grav1 (unused), flattening (unused)
+	double planet_params[] = {200,0.1,0.15,500.0,0.05}; // grid size, impact, rprs, semimajor, obliquity
+	double phase[50];
+	for(int a = 0; a < 50; a++){
+		phase[a] = asin(((a-25.0)/25.0)*1.5/500.0);
+		printf("P:%f\n",phase[a]);
+	}
+	SimpleModel model;
+	model.SetupStar(star_params,5);
+	model.SetupPlanet(planet_params,5);
 
+	double *flux_out = (double*)calloc(50,sizeof(double));
+	model.RelativeFlux(phase,50,flux_out,50);
+	printf("FLUX OUTPUT ======\n");
+	for(int a=0;a<50;a++){
+		printf("%i %f\n",a,flux_out[a]);
+	}
+	free(flux_out);
 	return 0;
 }
--- transitmodel.h
+++ transitmodel.h
@@ -6,17 +6,46 @@
 #include<time.h>
 #include<gsl/gsl_sf_ellint.h>
 #define pi 3.1415926535897932384626433832795028841971693993751
+
+// SetupStar input parameter keys
+// **********************
+// Model 1
+// **********************
+#define KEY_SS_GRID_SIZE 0 // Star setup -- number of grid points per row
+#define KEY_SS_LIMB_DARKENING_1 1 // Star setup -- stellar limb darkening 1
+#define KEY_SS_LIMB_DARKENING_2 2 // Star setup -- stellar limb darkening 2
+#define KEY_SS_GRAVITY_DARKENING_1 3 // Star setup -- stellar gravity darkening profile
+#define KEY_SS_FLATTENING 4 // Star setup -- stellar flattening
+// **********************
+
+
+// PlanetSetup input parameter keys
+// **********************
+// **********************
+#define KEY_PS_GRID_SIZE 0 // Planet setup -- number of points in grid (TODO?)
+#define KEY_PS_IMPACT 1 // Planet setup -- impact parameter
+#define KEY_PS_RPRS 2 // Planet setup -- Radius of planet / radius of star
+#define KEY_PS_SEMI_MAJOR_AXIS 3 // Planet setup -- semi major axis in R star
+#define KEY_PS_OBLIQUITY 4 // Planet setup -- obliquity
+#define KEY_PS_ECCENTRICITY 5 // Planet setup -- eccentricity
+// **********************
+
+
 using namespace std;
-class Transitmodel{
+class TransitModel{
   public:
-    Transitmodel();
-    ~Transitmodel();
+    TransitModel(){};
+    ~TransitModel(){};
     //stellar shape, limb darkening profile, gravity darkening profile
-    void Setup_star(double *star_params, int np);
+    void SetupStar(double *star_params, int np);
     //planet shape, orbit period, impact parameter/inclination,eccentricity
-    void Setup_planet(double *planet_params, int np);
-    void relativeFlux(double *phase, int np, double *deficitFlux, int nf);
+    void SetupPlanet(double *planet_params, int np);
+    void RelativeFlux(double *phase, int np, double *deficit_flux, int nf);
   private:
-    
+    double *star_flux_map; // 2-d flux map
+    double *planet_oppacity_map; // 2-d oppacity map
+    int planet_grid_size;
+    int star_grid_size;
+    double planet_radius; // in Rp/Rs
 };
 #endif
