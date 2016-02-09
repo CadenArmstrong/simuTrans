826d4b2c3b154aebb2652e09ef9af67dfa4e1ff5
CadenArmstrong <cadenarmstrong@gmail.com>
2016-02-08 20:41:12 -0500

    Bug fixing in Simple model.

--- simplemodel.cpp
+++ simplemodel.cpp
@@ -34,32 +34,34 @@ void SimpleModel::SetupStar(double *star_params, int np){
 		this->star_grid_size += 1;
 	}
 	this->star_grid_size_half = (this->star_grid_size-1)/2;
-	this->star_flux_map = (double*)calloc((star_params[KEY_SS_GRID_SIZE]*star_params[KEY_SS_GRID_SIZE]),sizeof(double));
+	this->star_flux_map = (double*)calloc((this->star_grid_size*this->star_grid_size),sizeof(double));
 
 	float mu = 0;
 	long double total_flux = 0;
 	for(int x=0;x<this->star_grid_size;x++){
 		for(int y = 0;y<this->star_grid_size;y++){
-			mu = sqrt(x*x + y*y)/star_grid_size;
+			mu = sqrt(pow(x-this->star_grid_size_half,2)+ pow(y-this->star_grid_size_half,2))/star_grid_size_half;
 			// QUADRATIC LIMB DARKENING LAW
 			if(mu <= 1.0){
-				this->star_flux_map[x+y*this->star_grid_size] =  1-star_params[KEY_SS_LIMB_DARKENING_1]*(1-mu)-star_params[KEY_SS_LIMB_DARKENING_2]*(1-mu)*(1-mu);
+				this->star_flux_map[x+y*this->star_grid_size] =  1.0-(star_params[KEY_SS_LIMB_DARKENING_1]*(mu))-(star_params[KEY_SS_LIMB_DARKENING_2]*mu*mu);
 				total_flux += star_flux_map[x+y*this->star_grid_size]*this->star_pixel_size;
 			}else{
 				this->star_flux_map[x+y*this->star_grid_size] =  0;
 			}
+			//printf("%f ",this->star_flux_map[x+y*this->star_grid_size]);
 		}
+//		printf("\n");
 	}
 	this->star_total_flux = (double)total_flux;
 	printf("Star setup complete\n");
 };
 void SimpleModel::SetupPlanet(double *planet_params, int np){
 	this->planet_grid_size = (int)planet_params[KEY_PS_GRID_SIZE];
-	this->planet_oppacity_map = (double*)calloc((planet_params[KEY_SS_GRID_SIZE]*planet_params[KEY_PS_GRID_SIZE]),sizeof(double));
 	if(this->planet_grid_size % 2 == 0){
 		printf("Even sized grid detected, moving to odd size\n");
 		this->planet_grid_size += 1;
 	}
+	this->planet_oppacity_map = (double*)calloc((this->planet_grid_size*this->planet_grid_size),sizeof(double));
 	this->planet_grid_size_half = (this->planet_grid_size-1)/2;
 	this->rp_rs = planet_params[KEY_PS_RPRS];
 	this->planet_pixel_size = this->rp_rs*this->rp_rs;
@@ -69,7 +71,7 @@ void SimpleModel::SetupPlanet(double *planet_params, int np){
 	double mu;
 	for(int x=0;x<this->planet_grid_size;x++){
 		for(int y = 0;y<this->planet_grid_size;y++){
-			mu = sqrt(x*x + y*y)/planet_grid_size;
+			mu = sqrt(pow(x-this->planet_grid_size_half,2) + pow(y-this->planet_grid_size_half,2))/planet_grid_size_half;
 			if(mu <= 1.0){
 				this->planet_oppacity_map[x+y*this->planet_grid_size] =  1.0;
 			}else{
@@ -86,7 +88,7 @@ void SimpleModel::RelativeFlux(double *phase, int np, double *flux_out, int npo)
 	double planet_position_y = 0;
 	double current_flux = 0;
 	for(int a=0; a<np;a++){
-		printf("Phase: %f\n",phase[a]);
+		current_flux = 0;
 		planet_position_x = this->semi_major*sin(phase[a]);
 		planet_position_y = this->impact_parameter + tan(this->obliquity)*planet_position_x;
 		for(int x=0;x<this->planet_grid_size;x++){
@@ -98,8 +100,7 @@ void SimpleModel::RelativeFlux(double *phase, int np, double *flux_out, int npo)
 				}
 			}
 		}
-		flux_out[a] = this->star_total_flux - current_flux;
-		printf("TEST\n");
+		flux_out[a] = (this->star_total_flux - current_flux)/this->star_total_flux;
 	}
 	printf("Flux integration complete\n");
 };
--- simutrans.cpp
+++ simutrans.cpp
@@ -2,12 +2,11 @@
 #include "simplemodel.cpp"
 
 int main(int argc, char *argv[]){
-	double star_params[] = {350,0.5,0.6,0.1,0.95}; // grid size, limb1, limb2, grav1 (unused), flattening (unused)
+	double star_params[] = {5000,0.5,0.2,0.1,0.95}; // grid size, limb1, limb2, grav1 (unused), flattening (unused)
 	double planet_params[] = {200,0.1,0.15,500.0,0.05}; // grid size, impact, rprs, semimajor, obliquity
 	double phase[50];
 	for(int a = 0; a < 50; a++){
-		phase[a] = asin(((a-25.0)/25.0)*1.5/500.0);
-		printf("P:%f\n",phase[a]);
+		phase[a] = asin(((a-25.0)/25.0)*0.75/500.0);
 	}
 	SimpleModel model;
 	model.SetupStar(star_params,5);
@@ -17,7 +16,7 @@ int main(int argc, char *argv[]){
 	model.RelativeFlux(phase,50,flux_out,50);
 	printf("FLUX OUTPUT ======\n");
 	for(int a=0;a<50;a++){
-		printf("%i %f\n",a,flux_out[a]);
+		printf("%i %.9f\n",a,flux_out[a]);
 	}
 	free(flux_out);
 	return 0;
